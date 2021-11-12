1b1dfb9ea426a16885c41aa3fc362e4fc110c0f2
Brandon Eads <brandon181989@hotmail.com>
2021-11-12 09:09:03 -0800

    solved q1 w/o recursion

--- 1-StringURL/StringURL.js
+++ 1-StringURL/StringURL.js
@@ -12,5 +12,18 @@
 // Create a function that utilizes .split() with space as the separator then join with %20
 // *Researching mozilla .split() docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
+// Solved and refactored on codebunk https://codebunk.com/b/1451100377891/
+
+const string = "Jasmine Ann Jones"; // Set an input example
+
+function splitString(stringSplit) { // Create function that takes an argument
+    const arrayString = stringSplit.split(" ") // Take the argument and split into an array using spaces as the separator
+    return arrayString.join("%20") // Rejoin the string using %20 instead of spaces
+}
+
+splitString(string) // Call the function with input example "string" as the argument
+
+
+
 // Recursive:
 // Rereading curriculum to get better understanding of recursion https://www.learnhowtoprogram.com/react/functional-programming-with-javascript/recursion
