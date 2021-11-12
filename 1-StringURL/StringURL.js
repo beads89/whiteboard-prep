d953294f7d639e824f68090b2f1dfc3b8a28e31a
Brandon Eads <brandon181989@hotmail.com>
2021-11-12 08:54:38 -0800

    add notes for q1, reading mozilla docs

--- 1-StringURL/StringURL.js
+++ 1-StringURL/StringURL.js
@@ -7,3 +7,10 @@
 // Input: "Jasmine Ann Jones"
 
 // Output: "Jasmine%20Ann%20Jones"
+
+// Non-recursive:
+// Create a function that utilizes .split() with space as the separator then join with %20
+// *Researching mozilla .split() docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+
+// Recursive:
+// Rereading curriculum to get better understanding of recursion https://www.learnhowtoprogram.com/react/functional-programming-with-javascript/recursion
