62d2cb8fc2aae2e88aeaad662051c7b3e155eb5c
Brandon Eads <brandon181989@hotmail.com>
2021-11-12 12:17:47 -0800

    solve q3 non-recursive

--- 3-Compressing/CompressString.js
+++ 3-Compressing/CompressString.js
@@ -11,5 +11,28 @@
 // Sources: array.protoype.reduce() Mozilla docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce and curriculum https://www.learnhowtoprogram.com/react/functional-programming-with-javascript/map-reduce-and-filter
 // While loop: W3 Schools https://www.w3schools.com/js/js_loop_while.asp and Mozilla Docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
 
-const string = aaabccdddda;
 
+// Multiple refactors and deletions in codebunk https://codebunk.com/b/8381100378008/
+
+const string = "aaabccdddda";
+
+function compression(string) { // Create function that takes a string as an argument 
+  let stringOccurance = ""; // Set a string to return at the end
+  let stringArray = string.split(""); // Turn the string into an array
+  for (let i = 0; i < stringArray.length; i++) { // Run for loop on array
+    let count = 1; // Set default count of occurances to 1
+    let letter = stringArray[i]; // Define what letter is at the looping index
+    while (i < stringArray.length -1 && stringArray[i] === stringArray[i + 1]) { // Use while loop to snap shot consecutive matching numbers
+      count++; // Add another number to the occurance counter
+      i++ // Go through the while loop again if condition was met
+    }
+    if (count === 1) { // If letter only occured consecutively once
+      stringOccurance += letter // Only output the letter and no number
+    } else {
+      stringOccurance += count + letter; // If occured consecutively, output the number of times followed by the letter
+    }
+  }
+  return stringOccurance
+}
+
+console.log(compression(string)); // Input in console to confirm it works
